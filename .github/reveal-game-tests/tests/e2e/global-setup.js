aa143cdde4a8708cc68f6a316a0d5e94ce633d4f
Changhyun Choi <changhyun.choi.98@gmail.com>
2026-08-14 22:12:50 +0900

    Revert "feat: 이미지 공개 퀴즈 PWA 추가 (#6)"
    This reverts commit 630d72aab67888770b3af20244d6746d2548a79c.

--- .github/reveal-game-tests/tests/e2e/global-setup.js
+++
@@ -1,8 +0,0 @@
-const path = require("node:path");
-const { pathToFileURL } = require("node:url");
-
-module.exports = async () => {
-  const modulePath = path.resolve(__dirname, "../../scripts/generate-fixtures.mjs");
-  const { generateFixtures } = await import(pathToFileURL(modulePath));
-  await generateFixtures();
-};
